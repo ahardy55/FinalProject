528a1aa5904def029bab5524b5302eb0a1ecbaf7
Adam Hardy <ahardy55@gmail.com>
2013-06-10 12:34:00 -0400

    created container

--- h5bp-html5-boilerplate-3cd1967 2/js/main.js
+++ h5bp-html5-boilerplate-3cd1967 2/js/main.js
@@ -1,1 +1,10 @@
+$('.grades').click(function() { 
 
+	//add a class to the selected grade that makes the background red
+	$('.grades.selected_grade').removeClass('selected_grade');
+	$(this).addClass('selected_grade');
+
+	//put the value of the clicked grade in the user grade
+	var clicked_grade = $(this).val();
+	$('#user_grade').val(clicked_grade); 
+});
