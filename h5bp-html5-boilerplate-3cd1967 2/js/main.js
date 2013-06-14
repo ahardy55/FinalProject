73b421514b689c40d435cbd26340a028cf8dc828
Adam Hardy <ahardy55@gmail.com>
2013-06-14 15:33:48 -0400

    add Jquery animation

--- h5bp-html5-boilerplate-3cd1967 2/js/main.js
+++ h5bp-html5-boilerplate-3cd1967 2/js/main.js
@@ -9,3 +9,28 @@ $('.grades').click(function() {
 	$('#user_grade').val(clicked_grade); 
 });
 
+$(document).ready(function() {
+  $('#members').animate({ color: 'black',
+  }, 3000, function() { $('.approval').animate({ color: 'black',
+  }, 3000, function() {$('.election').animate({ color: 'black',
+  }, 3000, function() {$('.problem').animate({ color: 'black',
+  }, 3000, function() {
+  });
+  });
+  });
+  });
+});
+
+$(document).ready(function() {
+  $('#capitol').animate({ opacity: '1',
+  }, 3000, function() { $('#graph').animate({ opacity: '1',
+  }, 3000, function() {$('#nintypercent').animate({ opacity: '1',
+  }, 3000, function() {$('#questionmark').animate({ opacity: '1',
+  }, 3000, function() {
+  });
+  });
+  });
+  });
+});
+
+
