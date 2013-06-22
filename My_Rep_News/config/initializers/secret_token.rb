b074a3327a9a8de3dff12a98ecbba05cab3c0d13
Adam Hardy <ahardy55@gmail.com>
2013-06-22 16:30:06 -0400

    building into Rails

---
+++ My_Rep_News/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRepNews::Application.config.secret_token = '6f33dd14532241db293ee8678f30aed1ca58ac3dbde8912858d8c028955a8a5911d4b9dc5cf241b4490b4e2b86d7d632c56278d3bb628966c2f90e919f8c9794'
